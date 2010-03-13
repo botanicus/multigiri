18b29222790bfd3a44206906a327a011115db0ad
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-03-13 00:26:48 +0000

    Default attributes middleware

---
+++ lib/multigiri/default_attributes.rb
@@ -0,0 +1,22 @@
+# encoding: utf-8
+
+class Multigiri
+  class DefaultAttributes
+    def initialize(app, defaults = nil)
+      @app, @defaults = app, defaults
+      @defaults ||= {"form" => {"method" => "POST"}, "script" => {"type" => "text/javascript"}}
+    end
+
+    def call(env)
+      status, headers, document = @app.call(env)
+      @defaults.each do |tag, attributes|
+        document.css(tag).each do |element|
+          attributes.each do |attribute, value|
+            element[attribute] ||= value
+          end
+        end
+      end
+      [status, headers, document]
+    end
+  end
+end
