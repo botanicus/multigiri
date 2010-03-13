4218c21edaf53e6cc7c9ce6f5dfb6df8e25638aa
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-03-13 00:28:12 +0000

    Several improvements for link checker

--- lib/multigiri/link_checker.rb
+++ lib/multigiri/link_checker.rb
@@ -2,43 +2,54 @@
 
 # @example
 #   use Multigiri do
-#     use LinkChecker, Rango.logger do |path|
-#       "public" + path
+#     use LinkChecker, Logger.new("log/links.log") do |checker|
+#       def checker.path_from_link(link)
+#         "media" + link
+#       end
 #     end
 #   end
 class Multigiri
   class LinkChecker
     def initialize(app, logger, patterns = nil, &block)
-      @app, @logger, @patterns, @block = app, logger, patterns, block
+      @app, @logger, @patterns = app, logger, patterns
       @patterns ||= {"img" => "src", "link" => "href", "a" => "href"}
-    end
-
-    def links
-      @links ||= Array.new
+      block.call(self) unless block.nil?
     end
 
     def call(env)
+      @path_info = env["PATH_INFO"]
       status, headers, document = @app.call(env)
       @patterns.each do |tag, attribute|
         document.css("#{tag}[#{attribute}]").each do |element|
           link = element[attribute]
-          unless link.match(/^\w+:\/\//)
-            self.links << link
+          if link.match(/^\w+:\/\//)
+            check_remote_link(link)
+          else
+            check_local_link(link)
           end
         end
       end
-      check_links
       [status, headers, document]
     end
 
     private
-    def check_links
-      self.links.each do |link|
-        real_path = @block.call(link)
-        unless File.exist?(real_path)
-          @logger.error("[LINK] #{link} doesn't exist")
-        end
+    def path_from_link(link)
+      "public" + link
+    end
+
+    def check_local_link(link)
+      real_path = path_from_link(link)
+      unless File.exist?(real_path)
+        @logger.error("[LINK from #{@path_info}] #{link} doesn't exist in #{real_path}")
       end
+    end
+
+    def check_remote_link(link)
+      # require "net/http"
+      #
+      # Net::HTTP::Get.start() do |http|
+      #   http
+      # end
     end
   end
 end
