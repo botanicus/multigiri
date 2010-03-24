17a7c7b2229f89bdc136ba0bc8b1428efcee4cc0
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-03-24 11:51:50 +0000

    Multigiri -> Multigiri::HTML + Multigiri::XML. It perform better and has additional options such as content_types which can include an array of strings and regexp for matching if multigiri will run for current content type

--- lib/multigiri.rb
+++ lib/multigiri.rb
@@ -10,34 +10,81 @@
 require "nokogiri"
 
 # TODO: args for nokogiri, export format, indentation etc
-class Multigiri
-  attr_reader :options, :block
-  def initialize(app, options = Hash.new, &block)
-    @app, @options, @block = app, options, block
-    @options[:indentation] ||= 2
-  end
+module Multigiri
+  class HTML
+    # Ruby GC is weak
+    CONTENT_TYPE   = "Content-Type"
+    CONTENT_LENGTH = "Content-Length"
+    DEFAULT_TYPES  = ["text/html"]
 
-  def call(env)
+    attr_reader :options, :block
+    def initialize(app, options = Hash.new, &block)
+      @app, @options, @block = app, options, block
+      @options[:indentation]   ||= 2
+      @options[:content_types] ||= self.class::DEFAULT_TYPES
+    end
+
+    def call(env)
+      status, headers, chunks = @app.call(env)
+      check_content_type
+      if options[:content_types].empty? || options[:content_types].any? { |type| headers[CONTENT_TYPE].match(type) }
+        run(status, headers, chunks)
+      else
+        [status, headers, chunks]
+      end
+    end
+
     # convert to a Nokogiri document
-    status, headers, chunks = @app.call(env)
-    # the only what we know about body is that it has to respond to #each
-    body = String.new
-    chunks.each { |chunk| body += chunk }
-    document = Nokogiri::HTML(body) # before
-    @stack = lambda { |env| [status, headers, document] }
+    def run(status, headers, chunks)
+      # the only what we know about body is that it has to respond to #each
+      body = String.new
+      chunks.each { |chunk| body += chunk }
+      document = Nokogiri::HTML(body) # before
+      @stack = lambda { |env| [status, headers, document] }
 
-    # get middlewares
-    self.instance_eval(&@block) if @block
+      # get middlewares
+      self.instance_eval(&@block) if @block
 
-    # convert back to a [String]
-    status, headers, document = @stack.call(env)
-    body = document.to_html(indentation: options[:indentation]) # TODO
-    headers["Content-Length"] = body.bytesize.to_s
-    [status, headers, [body]]
+      # convert back to a [String]
+      status, headers, document = @stack.call(env)
+      body = document.to_html(indentation: options[:indentation]) # TODO
+      headers[CONTENT_LENGTH] = body.bytesize.to_s
+      return [status, headers, [body]]
+    end
+
+    # GoogleAnalytics.new(Other.new(self), tracking_code)
+    def use(klass, *args, &block)
+      @stack = klass.new(@stack, *args, &block)
+    end
+
+    protected
+    def check_content_type
+      if headers[CONTENT_TYPE].nil? && !options[:content_types].empty?
+        raise "Content-Type has to be set before you call multigiri, so multigiri can decide if it will parse the document or not!"
+      end
+    end
   end
 
-  # GoogleAnalytics.new(Other.new(self), tracking_code)
-  def use(klass, *args, &block)
-    @stack = klass.new(@stack, *args, &block)
+  class XML
+    # XML has many MIME types, if you want to work with all posible XML MIME types, you can use
+    # use Multigiri, mime_type: /xml/
+    DEFAULT_TYPES = ["text/xml"]
+
+    def run(status, headers, chunks)
+      # the only what we know about body is that it has to respond to #each
+      body = String.new
+      chunks.each { |chunk| body += chunk }
+      document = Nokogiri::XML(body) # before
+      @stack = lambda { |env| [status, headers, document] }
+
+      # get middlewares
+      self.instance_eval(&@block) if @block
+
+      # convert back to a [String]
+      status, headers, document = @stack.call(env)
+      body = document.to_xml(indentation: options[:indentation]) # TODO
+      headers[CONTENT_LENGTH] = body.bytesize.to_s
+      return [status, headers, [body]]
+    end
   end
 end
